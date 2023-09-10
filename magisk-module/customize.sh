37d9f13f1da34bbd00777d89d815eec609b54c54
Takeda-senpai <deadboltsx303@gmail.com>
2023-09-10 22:26:52 +0700

    Split Flavor

--- magisk-module/customize.sh
+++ magisk-module/customize.sh
@@ -23,19 +23,24 @@ elif [ "$ABI" == "x64" ] || [ "$ABI" == "x86_64" ]; then
   ABI32=x86
 fi
 
+FLAVOR=Normal
+
 [ ! -d "$TMPDIR/libs/$ABI" ] && abort "! $ABI not supported"
 {
     randomc="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 15)"
     ui_print "- Patch service: ${randomc}"
-    ui_print "- Install Zygisk libraries"
-    mkdir "$MODPATH/zygisk"
-    cp -af "$TMPDIR/libs/$ABI/proc_monitor" "$MODPATH/zygisk/$ABI.so"
-    cp -af "$TMPDIR/libs/$ABI32/proc_monitor" "$MODPATH/zygisk/$ABI32.so"
-    sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/zygisk/$ABI.so"
-    sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/zygisk/$ABI32.so"
-    ui_print "- Install execute binary"
-    cp -af "$TMPDIR/libs/$ABI/proc_monitor" "$MODPATH/proc_monitor"
-    sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/proc_monitor"
+    if [ "$FLAVOR" == "Zygisk" ]; then
+        ui_print "- Install Zygisk libraries"
+        mkdir "$MODPATH/zygisk"
+        cp -af "$TMPDIR/libs/$ABI/proc_monitor" "$MODPATH/zygisk/$ABI.so"
+        cp -af "$TMPDIR/libs/$ABI32/proc_monitor" "$MODPATH/zygisk/$ABI32.so"
+        sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/zygisk/$ABI.so"
+        sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/zygisk/$ABI32.so"
+    else
+        ui_print "- Install execute binary"
+        cp -af "$TMPDIR/libs/$ABI/proc_monitor" "$MODPATH/proc_monitor"
+        sed -i "s/proc_monitor__PLACEHOLDERNAME/proc_monitor__${randomc}/g" "$MODPATH/proc_monitor"
+    fi
     unzip -o "$ZIPFILE" post-fs-data.sh -d "$MODPATH"
 }
 
